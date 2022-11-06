818faf063b094c61c7da2153511423d1b2125cd7
nourmami <nour27mami@gmail.com>
2022-11-06 16:44:08 +0100

    classification & clusteringadded

---
+++ Classification/ClassificationModel.py
@@ -0,0 +1,165 @@
+import pandas as pd
+
+# load the training dataset
+diabetes = pd.read_csv('diabetes.csv')
+diabetes.head()
+
+# Separate features and labels
+features = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']
+label = 'Diabetic'
+X, y = diabetes[features].values, diabetes[label].values
+
+for n in range(0,4):
+    print("Patient", str(n+1), "\n  Features:",list(X[n]), "\n  Label:", y[n])
+
+#compare the feature distributions for each label value
+from matplotlib import pyplot as plt
+
+for col in features:
+    diabetes.boxplot(column=col, by='Diabetic', figsize=(6,6))
+    plt.title(col)
+plt.show()
+
+# Split the data into training and test sets
+from sklearn.model_selection import train_test_split
+
+# Split data 70%-30% into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print ('Training cases: %d\nTest cases: %d' % (X_train.shape[0], X_test.shape[0]))
+
+#logistic regression model
+# Train the model
+from sklearn.linear_model import LogisticRegression
+
+# Set regularization rate
+reg = 0.01
+
+# train a logistic regression model on the training set
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+print (model)
+
+# predict class labels for the test set
+predictions = model.predict(X_test)
+print('Predicted labels: ', predictions)
+print('Actual labels:    ' ,y_test)
+
+#evaluate the model accuracy
+from sklearn.metrics import accuracy_score
+
+print('Accuracy: ', accuracy_score(y_test, predictions))
+
+#other evaluations
+from sklearn. metrics import classification_report
+
+print(classification_report(y_test, predictions))
+
+#precision and recall 
+from sklearn.metrics import precision_score, recall_score
+
+print("Overall Precision:",precision_score(y_test, predictions))
+print("Overall Recall:",recall_score(y_test, predictions))
+
+
+#matrix of confusion
+from sklearn.metrics import confusion_matrix
+
+# Print the confusion matrix
+cm = confusion_matrix(y_test, predictions)
+print (cm)
+
+#You can use the predict_proba method to see the probability pairs for each case:
+
+
+
+y_scores=model.predict_proba(X_test)
+print(y_scores)
+
+#A common way to evaluate a classifier is to examine the true positive rate (which is another name for recall) and the false positive rate for a range of possible thresholds
+from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
+import matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# calculate ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+
+# plot ROC curve
+fig = plt.figure(figsize=(6, 6))
+# Plot the diagonal 50% line
+plt.plot([0, 1], [0, 1], 'k--')
+# Plot the FPR and TPR achieved by our model
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+#metric AUC
+from sklearn.metrics import roc_auc_score
+
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
+
+#preprocessing pipeline
+# Train the model
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+# Define preprocessing for numeric columns (normalize them so they're on the same scale)
+numeric_features = [0,1,2,3,4,5,6]
+numeric_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())])
+
+# Define preprocessing for categorical features (encode the Age column)
+categorical_features = [7]
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# Combine preprocessing steps
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+# Create preprocessing and training pipeline
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('logregressor', LogisticRegression(C=1/reg, solver="liblinear"))])
+
+
+# fit the pipeline to train a logistic regression model on the training set
+model = pipeline.fit(X_train, (y_train))
+print (model) 
+
+#test with the pipeline model
+# Get predictions from test data
+predictions = model.predict(X_test)
+y_scores = model.predict_proba(X_test)
+
+# Get evaluation metrics
+cm = confusion_matrix(y_test, predictions)
+print ('Confusion Matrix:\n',cm, '\n')
+print('Accuracy:', accuracy_score(y_test, predictions))
+print("Overall Precision:",precision_score(y_test, predictions))
+print("Overall Recall:",recall_score(y_test, predictions))
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
+
+# calculate ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+
+# plot ROC curve
+fig = plt.figure(figsize=(6, 6))
+# Plot the diagonal 50% line
+plt.plot([0, 1], [0, 1], 'k--')
+# Plot the FPR and TPR achieved by our model
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
