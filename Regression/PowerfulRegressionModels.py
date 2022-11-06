e43726bb8a5bd3998ba5b616e210e2dadc9ebbdb
nourmami <nour27mami@gmail.com>
2022-11-06 01:55:34 +0100

    newchapter3

---
+++ Regression/PowerfulRegressionModels.py
@@ -0,0 +1,143 @@
+#Let's start by loading the bicycle sharing data as a Pandas DataFrame and viewing the first few rows. We'll also split our data into training and test datasets.
+
+# Import modules we'll need for this notebook
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the training dataset
+bike_data = pd.read_csv('daily-bike-share.csv')
+bike_data['day'] = pd.DatetimeIndex(bike_data['dteday']).day
+numeric_features = ['temp', 'atemp', 'hum', 'windspeed']
+categorical_features = ['season','mnth','holiday','weekday','workingday','weathersit', 'day']
+bike_data[numeric_features + ['rentals']].describe()
+print(bike_data.head())
+
+
+# Separate features and labels
+# After separating the dataset, we now have numpy arrays named **X** containing the features, and **y** containing the labels.
+X, y = bike_data[['season','mnth', 'holiday','weekday','workingday','weathersit','temp', 'atemp', 'hum', 'windspeed']].values, bike_data['rentals'].values
+
+# Split data 70%-30% into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print ('Training Set: %d rows\nTest Set: %d rows' % (X_train.shape[0], X_test.shape[0]))
+
+#Try Another Linear Algorithm
+#Let's try training our regression model by using a Lasso algorithm. We can do this by just changing the estimator in the training code.
+from sklearn.linear_model import Lasso
+
+# Fit a lasso model on the training set
+model = Lasso().fit(X_train, y_train)
+print (model, "\n")
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
+
+### Try a Decision Tree Algorithm
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import export_text
+
+# Train the model
+model = DecisionTreeRegressor().fit(X_train, y_train)
+print (model, "\n")
+
+# Visualize the model tree
+tree = export_text(model)
+print(tree)
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
+
+###TRY an ensembile Algo
+from sklearn.ensemble import RandomForestRegressor
+
+# Train the model
+model = RandomForestRegressor().fit(X_train, y_train)
+print (model, "\n")
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
+
+
+###For good measure, let's also try a boosting ensemble algorithm. We'll use a Gradient Boosting estimator, which like a Random Forest algorithm builds multiple trees, but instead of building them all independently and taking the average result, each tree is built on the outputs of the previous one in an attempt to incrementally reduce the loss (error) in the model.
+# Train the model
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Fit a lasso model on the training set
+model = GradientBoostingRegressor().fit(X_train, y_train)
+print (model, "\n")
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
